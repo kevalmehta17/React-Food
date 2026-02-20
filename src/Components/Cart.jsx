c50188701c0fc9ccdef3088b8e8f1d64750b3b75
keval <kevalemhta885@gmail.com>
2026-02-20 17:00:00 +0530

    fix: Esc key bug

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -23,7 +23,7 @@ export default function Cart() {
   console.log("inside Cart component", cartCtx.items);
 
   return (
-    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"} onClose={userProgressCtx.progress === "cart" ? handleCloseCart : null}>
       <h2>Your cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -19,7 +19,7 @@ function Checkout() {
   }
 
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'}>
+    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
       <form>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
